13ada4ef62c4a8a17ccbb9f4e6b6b2a2c5ed868f
Chris Asche <chris.asche@gmail.com>
2015-11-30 16:05:02 -0400

    adds bootstrap and a nav bar with a single button

--- src/client/app/app.ts
+++ src/client/app/app.ts
@@ -4,7 +4,7 @@ import {Component, View, bootstrap} from 'angular2/angular2';
   selector: 'my-app'
 })
 @View({
-  template: '<h1>My first Angular 2 App</h1>'
+  templateUrl: 'app/app.html'
 })
 class AppComponent {
 }
